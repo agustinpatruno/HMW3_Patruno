c0a1b39ebde7dc4989ca8aa9012213bc4d6ddc9c
agustinpatruno <agustinpatruno@gmail.com>
2025-05-17 11:31:50 -0300

    correcto funcionamiento del ejercicio 2

--- ejercicio2/sources/main.cpp
+++ ejercicio2/sources/main.cpp
@@ -1,26 +1,38 @@
 #include "clase_procesador.hpp"
 
+/*
+    comando para compilar:
+        g++ -I../includes *.cpp -o programa
+    comando para ejecutar:
+        ./programa
+
+*/
+
 int main()
 {
 
     // muestra del funcionamiento de cada clase de formas //
-
+  
     Punto punto1(2,4);
+    cout << "informacion del objeto punto" << endl;
     cout << "valor en x: " << punto1.Getx() << endl;
     cout << " valor en y: " << punto1.Gety() << endl;
 
     Circulo circulo1(1,2, 5.6f);
+    cout << "informacion del objeto circulo" << endl;
     cout << "valor en x: " << circulo1.Get_posx() << endl;
     cout << "valor en y: " << circulo1.Get_posy() << endl;
     cout << "radio: " << circulo1.GetRadio() << endl;
 
     Elipse elipse1(3,6,5,7);
+    cout << "informacion del objeto elipse" << endl;
     cout << "valor en x: " << elipse1.Get_posx() << endl;
     cout << "valor en y: " << elipse1.Get_posy() << endl;
     cout << "semieje a: " << elipse1.Get_a() << endl;
     cout << "semieje b: " << elipse1.Get_b() << endl;
 
     Rectangulo rectangulo1(5, 7, 12.0f, 11.0f);
+    cout << "informacion del objeto rectangulo" << endl;
     cout << "valor en x: " << rectangulo1.Get_verx() << endl;
     cout << "valor en y: " << rectangulo1.Get_very() << endl;
     cout << " Largo: " << rectangulo1.Get_largo() << endl;
@@ -33,9 +45,8 @@ int main()
     float area_rectangulo = ProcesadorFigura<Rectangulo>::Area(rectangulo1.Get_largo(), rectangulo1.Get_ancho());
 
     cout << "area del circulo1: " << area_circulo << endl;
-    cout << " area del elipse: " << area_elipse << endl;
-    cout << " area del rectangulo: " << area_rectangulo << endl;
+    cout << " area del elipse1: " << area_elipse << endl;
+    cout << " area del rectangulo1: " << area_rectangulo << endl;
 
     return 0;
-
 }
